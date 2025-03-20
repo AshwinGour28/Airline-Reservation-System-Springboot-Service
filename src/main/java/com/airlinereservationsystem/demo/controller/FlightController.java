d493fc8680390ca167ff8079f2f69bbe0e224307
AshwinGour28 <ashwing2884@gmail.com>
2025-03-20 21:44:53 +0530

    complete flight sevice functionality

---
+++ src/main/java/com/airlinereservationsystem/demo/controller/FlightController.java
@@ -0,0 +1,43 @@
+package com.airlinereservationsystem.demo.controller;
+
+import com.airlinereservationsystem.demo.dto.Flight;
+import com.airlinereservationsystem.demo.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/flights")
+public class FlightController {
+
+    @Autowired
+    FlightService flightService;
+
+    @GetMapping
+    List<Flight> getAllFlights(@RequestParam(required = false, defaultValue = "asc", name = "sort") String sort){
+        return flightService.getAllFlights(sort);
+    }
+
+    @PostMapping
+    Flight createFlight(@RequestBody Flight flight){
+        return flightService.addFlight(flight);
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Flight> getFlightById(@PathVariable String id){
+        System.out.println("controller "+id);
+        try{
+            return ResponseEntity.ok().body(flightService.getFlightById(id));
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}/schedules")
+    public List<Flight> getFlightSchedules( @PathVariable String id, @RequestParam(required = false) List<String> dates) {
+
+        return flightService.getFlightSchedules(id, dates);
+    }
+}
---
+++ src/main/java/com/airlinereservationsystem/demo/service/FlightService.java
@@ -0,0 +1,33 @@
+package com.airlinereservationsystem.demo.service;
+
+import com.airlinereservationsystem.demo.dto.Flight;
+import com.airlinereservationsystem.demo.repository.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
+@Service
+public class FlightService {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    public List<Flight> getAllFlights(@RequestParam(required = false, defaultValue = "asc", name = "sort") String sort){
+        return flightRepository.getAllFlights(sort);
+    }
+
+    public Flight addFlight(Flight flight){
+        return flightRepository.addFlight(flight);
+    }
+
+    public Flight getFlightById(@PathVariable String id){
+        System.out.println("service "+id);
+        return flightRepository.getFlightById(id);
+    }
+
+    public List<Flight> getFlightSchedules(String flightId, List<String> dates){
+        return flightRepository.getFlightSchedules(flightId,dates);
+    }
+}
